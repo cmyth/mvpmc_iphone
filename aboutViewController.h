8ca258564a0b08b41568a8f08140263a86653fec
Jon Gettler <gettler@mvpmc.org>
2009-12-19 19:55:34 -0600

    Add an about view.

---
+++ aboutViewController.h
@@ -0,0 +1,16 @@
+//
+//  aboutViewController.h
+//  mvpmc
+//
+//  Created by Jon Gettler on 12/19/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface aboutViewController : UIViewController {
+
+}
+
+@end
